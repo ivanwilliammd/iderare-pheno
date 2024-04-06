8dbaacb5476a436f629768bb463881917caf60e0
ivanwilliammd <ivanwilliam.md@gmail.com>
2024-04-06 15:56:31 +0700

    added release.bat

---
+++ scripts/release.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+call conda activate iderare-pheno
+
+set TAG=
+for /f "tokens=*" %%a in ('python -c "from iderare_pheno.version import VERSION; print('v' + VERSION)"') do set "TAG=%%a"
+
+set /p prompt="Creating new release for !TAG!. Do you want to continue? [Y/n] "
+
+if /i "!prompt!"=="y" (
+    python scripts/prepare_changelog.py
+    git add -A
+    git commit -m "Bump version to !TAG! for release" || (echo "Nothing to commit" && exit /b 0)
+    git push
+    echo Creating new git tag !TAG!
+    git tag !TAG! -m "!TAG!"
+    git push --tags
+) else (
+    echo Cancelled
+    exit /b 1
+)
